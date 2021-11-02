b2e7f39375641a0b54b7a0adb89208e2773dea73
Leporoni <leporoni@hotmail.com>
2021-11-02 12:06:00 -0300

    Created repository, service and save method on service impl

---
+++ src/main/java/com/leporonitech/server/repository/ServerRepository.java
@@ -0,0 +1,8 @@
+package com.leporonitech.server.repository;
+
+import com.leporonitech.server.model.Server;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServerRepository extends JpaRepository<Server, Long> {
+    Server findByIpAddress(String ipAddress);
+}
