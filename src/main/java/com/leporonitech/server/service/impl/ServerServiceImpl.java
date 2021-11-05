9202e094bd1f9f0577575907ffdfa191961916c6
Leporoni <leporoni@hotmail.com>
2021-11-05 11:45:32 -0300

    Implementing ping() and list() methods on ServerService

--- src/main/java/com/leporonitech/server/service/impl/ServerServiceImpl.java
+++ src/main/java/com/leporonitech/server/service/impl/ServerServiceImpl.java
@@ -5,10 +5,19 @@ import com.leporonitech.server.repository.ServerRepository;
 import com.leporonitech.server.service.ServerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.io.IOException;
+import java.net.InetAddress;
 import java.util.Collection;
+
+import static com.leporonitech.server.enumeration.Status.SERVER_DOWN;
+import static com.leporonitech.server.enumeration.Status.SERVER_UP;
+import static java.util.List.of;
+
 @RequiredArgsConstructor
 @Service
 @Transactional
@@ -25,13 +34,19 @@ public class ServerServiceImpl implements ServerService {
     }
 
     @Override
-    public Server ping(String ipAddress) {
-        return null;
+    public Server ping(String ipAddress) throws IOException {
+        log.info("Pinging server IP: {}", ipAddress);
+        Server server = repository.findByIpAddress(ipAddress);
+        InetAddress address = InetAddress.getByName(ipAddress);
+        server.setStatus(address.isReachable(10000) ? SERVER_UP : SERVER_DOWN);
+        repository.save(server);
+        return server;
     }
 
     @Override
     public Collection<Server> list(int limit) {
-        return null;
+        log.info("Fetching all servers");
+        return repository.findAll((Pageable) of(0, limit)).toList();
     }
 
     @Override
